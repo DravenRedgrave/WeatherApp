f61d86b809a74e00e48cbea02ba4da075fd077cf
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2022-02-03 21:24:43 +0300

    Add files via upload

---
+++ src/Pages/Home.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import {useLazyQuery} from '@apollo/client';
+import {GET_WEATHER_QUERY} from '../graphql/Queries'
+
+function Home() {
+    const [citySearched, setCitySearched] = useState('')
+    const [getWeather,{loading,data,error}] = useLazyQuery(GET_WEATHER_QUERY, {
+        variables: {name: citySearched },
+    });
+
+    if (error) return <h1>Error found</h1>
+    if (data){
+        console.log(data);
+    }
+
+  return (
+    <div className='home'>
+        <h1>Search For Weather</h1>
+        <input type="text" placeholder='City name...' onChange={(event)=>{setCitySearched(event.target.value)}}/>
+        <button onClick={()=> getWeather()}>Search</button>
+        <div className='weather'>
+
+        </div>
+    </div>
+  );
+}
+
+export default Home;
